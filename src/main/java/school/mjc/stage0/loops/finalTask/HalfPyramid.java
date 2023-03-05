02268bc90d03dce36cbc59fc3f3db2bf9abf66bb
Yerke Nurgali <70740293+qelephant@users.noreply.github.com>
2023-03-05 21:39:32 +0600

    Update HalfPyramid.java

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -11,7 +11,7 @@ public class HalfPyramid {
             }
             for(j=0; j<=i; j++)
             {
-                System.out.print("* ");
+                System.out.print(" * ");
             }
             System.out.println();
         }
