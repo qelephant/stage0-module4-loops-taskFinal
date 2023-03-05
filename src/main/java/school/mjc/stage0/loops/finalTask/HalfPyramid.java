b62894c4fe2df0e47f8485f513e22cf844c63ac7
Yerke Nurgali <70740293+qelephant@users.noreply.github.com>
2023-03-05 21:37:12 +0600

    Update HalfPyramid.java

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,7 +2,19 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
-
+        int i, j;
+        for(i=0; i<cathetusLength; i++)
+        {
+            for(j=2*(cathetusLength-i); j>=0; j--)
+            {
+                System.out.print(" ");
+            }
+            for(j=0; j<=i; j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
